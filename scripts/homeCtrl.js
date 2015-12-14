b73fcf4984c1c8caaaa9e041bcb18034e0f0acd9
tejashreeKate <teju.kate@gmail.com>
2015-12-14 12:59:23 -0500

    update loction in map

--- scripts/homeCtrl.js
+++ scripts/homeCtrl.js
@@ -5,12 +5,12 @@ app.controller("HomeCtrl",function ($scope,$location){
 	$scope.goTo = function(url){
 		$location.path(url);
 	}
-	$scope.map = { center: { latitude: 40.7140, longitude: 74.0710 }, zoom: 15 };
+	$scope.map = { center: { latitude: 40.728157, longitude: -74.077642 }, zoom: 15 };
 	$scope.marker = {
       id: 0,
       coords: {
-        latitude: 40.7140,
-        longitude: 74.0710
+        latitude: 40.728157,
+        longitude: -74.077642
       }
     }
    
